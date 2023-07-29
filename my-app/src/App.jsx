3327775f531dcd5feabb91bbd9a79b7f1434585a
keep <flymayday112@gmail.com>
2023-07-29 20:52:29 +0800

    react

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+<div>
+    <ul className='comment'>
+<li>react</li>
+<li>react</li>
+<li>react</li>
+</ul>
+</div>
+)}
+
+export default App;
