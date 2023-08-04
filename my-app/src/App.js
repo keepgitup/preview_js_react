3540e7071385e3849b52f4bb61e1e2345badeb02
keep <flymayday112@gmail.com>
2023-08-04 20:57:54 +0800

    update emmet & react app.js change

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+   <ul className="comments">
+    <li>3q</li>
+    <li>I do miss u</li>
+    <li>now or never</li>
+   </ul>
+
+
+    );
+}
+
+export default App;
