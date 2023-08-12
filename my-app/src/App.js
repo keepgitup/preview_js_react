249c0f13697c1432e13a1affc2ab87e69262cd27
keep <flymayday112@gmail.com>
2023-08-12 16:10:24 +0800

    react router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,79 @@
-// import logo from './logo.svg';
-import './App.css';
-import React from 'react'
+import { Link, Outlet, Route, Routes } from 'react-router-dom';
 
-function App() {
-    const comments =["Thank you","3q","2","hey"];
-    return (
-   <ul className="comments">
-    <li>{comments[0]}</li>
-    <li>{comments[1]}</li>
-    <li>{comments[2]}</li>
-   </ul>
-
-
-    );
+export default function App() {
+  return (
+    <div>
+      <h1>Basic Example</h1>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='*' element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/dashboard'>Dashboard</Link>
+          </li>
+          <li>
+            <Link to='/nothing-here'>Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to='/'>Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
