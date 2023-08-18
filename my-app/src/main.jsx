ab724818a5465083735487e5ca4e954708f1c0ae
keep <flymayday112@gmail.com>
2023-08-18 20:57:31 +0800

    suffering biging to set up react env

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,3 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import{
+  createBrowserRouter,
+  RouterProvider,
+}from "react-router-dom";
+import "./index.css";
+
 [
     /* other routes */
     {
