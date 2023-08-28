378af9941ba478ab38e9c94b3986114cd9021e78
keep <flymayday112@gmail.com>
2023-08-28 15:54:13 +0800

    practice env setup

---
+++ react-type-backendtable/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+      <div className='App'>
+        組件
+      </div>
+
+
+  )
+}
+
+export default App
