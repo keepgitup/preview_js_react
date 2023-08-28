e86409553d96208ffc2e523e9c7d01c0de3f9d35
keep <flymayday112@gmail.com>
2023-08-28 18:35:40 +0800

    react create init file

--- react-type-backendtable/src/main.tsx
+++ react-type-backendtable/src/main.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+//正確的樣式引入順序
+//初始化樣式一般放在最前面
+import "reset-css"
+//UI框架的樣式
+
+//全局樣式
+import "./assets/styles/global.scss"
+
+//組件的樣式
 import App from './App.tsx'
 // import './index.css'
 
